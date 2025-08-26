3592ab3d789d8f0f421215001eda909329467190
Cheryl Mbani <mbanicheryl45@gmail.com>
2025-08-26 11:49:44 +0300

    Adding Organizer class and its relationship to other classes

--- lib/db/models.py
+++ lib/db/models.py
@@ -3,3 +3,20 @@ from sqlalchemy.orm import sessionmaker, declarative_base, relationship
 
 #Connect classes to the database using Base
 Base = declarative_base()
+
+
+session_participant = Table(
+    "session_participant",
+    Base.metadata,
+    Column("session_id", Integer, ForeignKey("sessions.id")),
+    Column("participant_id", Integer, ForeignKey("participants.id"))
+)
+
+class Organizer(Base):
+    __tablename__ = "organizers"
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String(40), nullable = False)
+    last_name = Column(String(40), nullable = False)
+    email_address = Column(String)
+    phone_number = Column(Integer, nullable = False)
+    sessions = relationship("Session", back_populates="organizer")
