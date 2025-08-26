72b223a8fc15b8b8bfcd16c659cd56c863abb61c
Cheryl Mbani <mbanicheryl45@gmail.com>
2025-08-26 11:53:35 +0300

    Adding the Session model and establishing its relationship to other models

--- lib/db/models.py
+++ lib/db/models.py
@@ -42,3 +42,29 @@ class Participant(Base):
     venue_id = Column(Integer, ForeignKey("venues.id"))
     sessions = relationship("Session", secondary= session_participant, back_populates="participants")
     venue = relationship("Venue", back_populates="participants")
+
+
+class Session(Base):
+    __tablename__= "sessions"
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    description = Column(Text)
+    organizer_id = Column(Integer, ForeignKey("organizers.id"), nullable=False)
+    venue_id = Column(Integer, ForeignKey("venues.id"), nullable=False)
+    organizer = relationship("Organizer", back_populates="sessions")
+    venue = relationship("Venue", back_populates="sessions")
+    participants = relationship("Participant", secondary= session_participant, back_populates="sessions")
+
+#Now connect the codes to the database
+engine = create_engine("sqlite:///mentorship.db")
+#Now lets create the tables in the database
+Base.metadata.create_all(bind=engine)
+#Communicate with the database
+Session = sessionmaker(bind=engine)
+session = Session()
+
+organizer1 = Organizer(first_name="Cheryl", last_name="Mbani",
+                       email_address="cheryl@gmail.com", phone_number=123456789)
+
+session.add(organizer1)
+session.commit()
