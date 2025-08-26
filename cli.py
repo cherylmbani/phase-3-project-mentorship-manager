0c6589b4798bcafdaae8692fd68e74302abdce0c
Cheryl Mbani <mbanicheryl45@gmail.com>
2025-08-26 16:40:01 +0300

    Initializing click to enable interaction with the python program via terminal

---
+++ cli.py
@@ -0,0 +1,8 @@
+import click
+from lib.db.crud import(
+    add_organizer,get_organizers, update_organizer, delete_organizer,
+    add_participant, get_participants, update_participant, delete_participant,
+    add_venue, get_venue, update_venue, delete_venue,
+    add_mentorship_session, get_mentorship_sessions, update_mentorship_session, delete_mentorship_session
+
+)
