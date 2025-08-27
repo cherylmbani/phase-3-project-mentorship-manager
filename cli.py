842bcd4613f89133be5d0c1467d5cf37b2ec691e
Cheryl Mbani <mbanicheryl45@gmail.com>
2025-08-27 10:07:28 +0300

    Retrieving and deleting mentorship sessions via CLI

--- cli.py
+++ cli.py
@@ -20,6 +20,7 @@ def main_menu():
         click.secho("2. Organizers", fg="white")
         click.secho("3. Participants", fg="white")
         click.secho("4. Venues", fg="white")
+        click.secho("5. Exit", fg="white")
         choice = click.prompt("Select an option", type=int)
         if choice == 1:
             click.secho("Choose the following mentorship session activities", fg="blue")
@@ -30,6 +31,7 @@ def main_menu():
             mentorship_choice = click.prompt("Select an option", type=int)
             if mentorship_choice==1:
                 click.secho("fill in the following fields of mentorship session to add", fg="red")
+                mentorship_session_id = click.prompt("Please enter the Id of the session to Update")
                 title=click.prompt("Session title")
                 date=click.prompt("Session date(YYYY-MM-DD)")
                 description=click.prompt("Session description")
@@ -38,9 +40,42 @@ def main_menu():
                 add_mentorship_session(title=title, date=date, description=description, organizer_id=organizer_id, venue_id=venue_id)
                 click.echo(f"{title} added successfully")
             if mentorship_choice==2:
+                click.secho("Update the following fields of mentorship", fg="red")
+                title=click.prompt("New session title", default="", show_default=False)
+                date=click.prompt("New session date, YYYY-MM-DD",default="", show_default=False)
+                description=click.prompt("New session description", default="", show_default=False)
+                organizer_id=click.prompt("New organizer Id", default="", show_default=False)
+                venue_id=click.prompt("New venue Id", default="", show_default=False)
+                #a dictionary to collect the new/updated data
+                updated_session={}
+                if title:
+                    updated_session["title"] = title
+                if date:
+                    updated_session["date"] = date
+                if description:
+                    updated_session["description"] = description
+                if organizer_id:
+                    updated_session["organizer_id"] = organizer_id
+                if venue_id:
+                    updated_session["venue_id"] = venue_id
+
                 
+                update_mentorship_session(mentorship_session_id, updated_session)
+                click.secho(f"Session {mentorship_session_id} is updated successfully")
+
+            if mentorship_choice==3:
+                click.echo("These are the sessions registered")
+                sessions = get_mentorship_sessions()
+                if not sessions:
+                    click.secho("Cannot retrieve sessions")
 
 
+        if choice ==5:
+            click.secho("Exiting..., Goodbye!", fg="magenta")
+            break
+
+if __name__=="__main__":
+    main_menu()
 
 
 
